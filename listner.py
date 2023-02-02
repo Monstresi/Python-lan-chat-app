d480a0d906c9c104ea7f09b937bac6abf79e80fb
Hassan <syedhassanali999@gmail.com>
2023-02-02 15:19:13 +1100

    Generate keys only

--- listner.py
+++ listner.py
@@ -1,7 +1,11 @@
 import socket
 import rsa
 
+# Encryption setup
+public_key, private_key = rsa.newkeys(1024)
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Connection made to get local ip
 s.connect(("192.168.1.1", 80))
@@ -19,9 +23,17 @@ s.listen(5)
 
 print("listening...")
 c, addr = s.accept()
+
+# Connection made
 print(f"Got connection from {addr}")
 
 c.send(b"Connected to the listener")
+
+# Send encryption public keyc.send(public_key.save_pkcs1())
+#print(f"Sent: {public_key.save_pkcs1()}")
+c.send(public_key.save_pkcs1())
+l_pub_key = c.recv(1024)
+#print(l_pub_key)
 
 while True:
     message = c.recv(1024)
@@ -34,6 +46,6 @@ while True:
     if message.decode() == "exit":
         break
     message = input("\u001b[0mEnter message to send: ")
-    c.send(bytes(message, 'utf-8'))
+    c.send(message.encode())
 
 c.close()
--- sender.py
+++ sender.py
@@ -1,6 +1,10 @@
 import socket
 import rsa
 
+# Encryption setup
+public_key, private_key = rsa.newkeys(1024)
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
@@ -9,18 +13,25 @@ port = 12345
 
 s.connect((host, port))
 
+# Connection made
 print(s.recv(1024).decode())
+
+# Receive listner public key
+l_pub_key = s.recv(1024)
+s.send(public_key.save_pkcs1())
 
 while True:
 
     message = input("\u001b[0mEnter message to send: ")
-    s.send(bytes(message, 'utf-8'))
+    #ciphertext = rsa.encrypt(message.encode(), public_key)
+
+    s.send(message.encode())
     if message == "exit":
         break
     message = s.recv(1024)
     if not message:
         break
     print(f"\u001b[34mReceived message: {message.decode()}")
-    if message.decode == "exit": s.close()
+    if message.decode() == "exit": break
 
 s.close()
